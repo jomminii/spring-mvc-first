b7aa258a432c905f187a029cc68a8149ade1d1eb
이종민 <leejm3@brandi.co.kr>
2022-03-25 02:09:04 +0900

    first1

---
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,44 @@
+package hello.itemservice.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+
+        //when
+        Item savedItem = itemRepository.save(item);
+
+        //then
+        Item findItem = itemRepository.findById(item.getId());
+
+        Assertions.assertThat()
+
+    }
+    @Test
+    void save() {
+        //given
+        //when
+        //then
+
+    }
+    @Test
+    void save() {
+        //given
+        //when
+        //then
+
+    }
+}
